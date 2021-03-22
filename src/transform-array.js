76439e0c8c75b730864830c60e07ea6329e5d7c7
EvvTim <evgeniytim94@gmail.com>
2021-03-22 16:58:58 +0100

    feat: add hanoi-tower and transform-array solutions

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  const controlSequences = [`--discard-next`, `--double-prev`, `--double-next`, `--discard-prev`]
+
+  return arr.reduce((prev, curr, index) => {
+    if (controlSequences.includes(curr)) return prev
+    if (arr[index - 1] === controlSequences[0]) return prev
+    if (arr[index - 1] === controlSequences[2]) {
+      prev.push(curr, curr);
+    } else {
+      prev.push(curr);
+    }
+    if (arr[index + 1] === controlSequences[3]) prev.splice(prev.length - 1, 1)
+    if (arr[index + 1] === controlSequences[1]) prev.push(curr)
+
+    return prev;
+  }, []);
+}
