82250f03aa591f44e4be17e4d95db32fc38258b5
EvvTim <evgeniytim94@gmail.com>
2021-03-22 14:00:30 +0100

    feat: add solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,18 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(arr) {
-  // remove line with error and write your code here
   let cats = []
   let mergedArr = [].concat.apply([],arr)
 
-  for (let i = 0; i <= mergedArr.length; i++) {
-    if (mergedArr[i] === '^^') {
-      cats.push(i)
-    } else {
-      return 0
-    }
-  }
+  mergedArr.map((el) => el === "^^" ? cats.push(el) : false)
 
   return cats.length
+}
 
-};
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,14 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(arr) {
-
-    if(!Array.isArray(arr)) return false
-
-    let teamArr = []
-
-    arr.map(item => {
-        if(typeof item  === 'string') teamArr.push(item[0].match(/[a-zA-Z]/).join('').toUpperCase())
-    })
-
-    return teamArr.sort().join('')
+    if (Array.isArray(arr) === false) return false
+    let newArr = []
+    arr.map((el) => typeof el === "string" ? newArr.push(el.trim().charAt(0).toUpperCase()) : false)
+    return newArr.length === 0 ? false : newArr.sort().join('')
 }
