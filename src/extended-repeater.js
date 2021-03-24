981d341f58eced282a538898c04eee1857017bdf
Evgeniy Tsimashchuk <evgeniytim94@gmail.com>
2021-03-24 20:03:14 +0100

    feat: add extended-repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+    let {
+        repeatTimes = 1,
+        separator = '+',
+        addition = '',
+        additionRepeatTimes = 1,
+        additionSeparator = '|'
+    } = options
+
+    if (repeatTimes === undefined) return ''
+
+    let separatorR = []
+    let strR = []
+
+    for (let i = 0; i < repeatTimes; i++) {
+        strR.push(str)
+    }
+
+    for (let i = 0; i < additionRepeatTimes; i++) {
+        separatorR.push(`${addition}`)
+    }
+
+    const addSeparatorStr = separatorR.join(additionSeparator)
+
+    console.log(addSeparatorStr)
+    console.log(separatorR)
+    console.log(strR)
+
+    strR = strR.map(el => el + addSeparatorStr)
+
+    return strR.join(separator)
+
+}
   
